e80228445549c0a07a15529161d342c6a3dc42ec
Nick Aberle <aberle.nick@gmail.com>
2014-10-29 19:29:20 -0600

    mongo stuff test

--- foodscrape.py
+++ foodscrape.py
@@ -23,8 +23,23 @@ dishes = parsed_html.body.find_all('div', attrs={'class' : 'flipwrapper'})
 
 chosen = choice(dishes)
 
-print chosen.attrs['data-sharetitle']
-print chosen.find('section', attrs={'class' : 'description'}).text
+'''print chosen.attrs['data-sharetitle']
+print chosen.find('section', attrs={'class' : 'description'}).text'''
 img = chosen.find('a', attrs={'class' : 'picture-link'})
-print img.attrs['href']
-print  img.find('img').attrs['src']
+'''print img.attrs['href']
+print img.find('img').attrs['src']'''
+
+recipe = { 'title' : chosen.attrs['data-sharetitle'],
+		   'description' : chosen.find('section', attrs={'class' : 'description'}).text,
+		   'link' : img.attrs['href'],
+		   'image' : img.find('img').attrs['src']
+		   }
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.recipeasy
+collection = db.recipes
+
+recipes = db.recipes
+recipes.insert(recipe)
